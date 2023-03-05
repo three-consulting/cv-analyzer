c78299e15de71bf38a993c5f67aa86cf884c51f1
Robert Siipola <robert@three.consulting>
2023-03-05 09:30:37 +0000

    feat: add search module

---
+++ app/search/service.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from typing import Optional
+from db.core import client, embedding_function
+from config import Settings
+
+
+def semantic_search(query: str, filter: Optional[str], k: int = 10):
+    collection = client.get_collection(
+        name=Settings().openai_collection_name,
+        embedding_function=embedding_function,
+    )
+
+    if filter:
+        res = collection.query(
+            query_texts=[query],
+            where_document={"$contains": filter},
+            n_results=k,
+        )
+    else:
+        res = collection.query(
+            query_texts=[query],
+            n_results=k,
+        )
+
+    result_df = pd.DataFrame(
+        {
+            "distances": list(res["distances"][0]),
+            "documents": list(res["documents"][0]),
+        }
+    )
+
+    return result_df
