17ed4f2c18b795b8d18e83b6699e2a4aad8550a4
Robert Siipola <robert@three.consulting>
2023-03-05 09:47:48 +0000

    feat: add semantic similarity search

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from loguru import logger
+import gradio as gr
+from search.service import semantic_search
 
 
-def main():
-    logger.add("../app.log", format="{time} {level} {message}", level="INFO")
+demo = gr.Interface(
+    fn=semantic_search,
+    inputs=[
+        gr.components.Textbox(label="query", lines=5),
+        gr.components.Textbox(label="filter", lines=5),
+        gr.components.Slider(5, 20, value=10, label="k", step=1),
+    ],
+    outputs=gr.components.DataFrame(
+        label="Semantically similar resumes", wrap=True
+    ),
+)
 
 
 if __name__ == "__main__":
-    main()
+    logger.add("app.log", format="{time} {level} {message}", level="INFO")
+    logger.info("Starting app...")
+    demo.launch()
